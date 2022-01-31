7f80805e47acfaba750ad27d7ec320c641bea6b0
central-station <stevetambussi@gmail.com>
2022-01-30 19:14:24 -0800

    Added speed_checker and top-level_launcher

---
+++ src/navigator/src/speed_checker.cpp
@@ -0,0 +1,30 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/MagneticField.h>
+#include <sensor_msgs/FluidPressure.h>
+#include <sensor_msgs/Temperature.h>
+
+/**
+ * Subscriber callbacks
+ */
+
+void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
+    ROS_INFO( "Accel: %.3f,%.3f,%.3f [m/s^2] - Ang. vel: %.3f,%.3f,%.3f [deg/sec] - Orient. Quat: %.3f,%.3f,%.3f,%.3f",
+              msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z,
+              msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z,
+              msg->orientation.x, msg->orientation.y, msg->orientation.z, msg->orientation.w);
+}
+
+/**
+ * Node main function
+ */
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "zed_sensors_subscriber");
+    ros::NodeHandle n;
+
+    ros::Subscriber subImu = n.subscribe("/zed2i/zed_node/imu/data", 10, imuCallback);
+
+    ros::spin();
+
+    return 0;
+}
