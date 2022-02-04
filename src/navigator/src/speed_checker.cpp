f0bf582fbd25baa3d59b86c70dacada3f035c7a6
central-station <stevetambussi@gmail.com>
2022-02-03 17:07:33 -0800

    More changes to speed checker

--- src/navigator/src/speed_checker.cpp
+++ src/navigator/src/speed_checker.cpp
@@ -1,19 +1,39 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
+#include <unistd.h>
+#include <chrono>
 
+float max_speed = 2.23; //in meters/s (~= 5mph)
+float max_var = 0.5; //experimental - greatest amount of variance allowed for variables
+auto start = std::chrono::steady_clock::now(); //timer check
+auto finish = std::chrono::steady_clock::now();
 //TODO: Calculate velocity from acceleration
-//TODO: Figure out how to filter values via covariance matrix (if it is not being done already)
-//TODO: put a limit on the robots speed (max speed is 5mph)
 
 /**
  * Subscriber callbacks
  */
 
 void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
+    /*
     ROS_INFO( "Accel: %.3f,%.3f,%.3f [m/s^2] - Ang. vel: %.3f,%.3f,%.3f [deg/sec] - Orient. Quat: %.3f,%.3f,%.3f,%.3f",
               msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z,
               msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z,
               msg->orientation.x, msg->orientation.y, msg->orientation.z, msg->orientation.w);
+    */
+
+    /* Linear Acceleration and Variance */
+    //ROS_INFO("Linear Accel: x = %.3f[m/s^2], y = %.3f[m/s^2]", msg->linear_acceleration.x, msg->linear_acceleration.y);
+    //Row major x
+    //ROS_INFO("Linear Accel Var: x[%.3f] [m/s^2]", msg->linear_acceleration_covariance[0]);
+    //Row major y
+    //ROS_INFO("Linear Accel Var: y[%.3f] [m/s^2]", msg->linear_acceleration_covariance[4]);
+
+    /* Angular Acceleration and Variance */
+    //ROS_INFO("Ang. vel. (z): %.3f [deg/sec]", msg->angular_velocity.z);
+    //Row major about z axis
+    //ROS_INFO("Ang. vel. Var: z[%.3f] [deg/sec]", msg->angular_velocity_covariance[8]);
+    //usleep(100000); //to slow down output
+    
 }
 
 /**
