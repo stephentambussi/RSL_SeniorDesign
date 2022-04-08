641a950f98f58c3d544d425bc519f67cc7440a1a
dylanhoover <dylangranthoover@gmail.com>
2022-04-08 13:46:45 -0700

    joystick

--- src/tablet-webpage/main.js
+++ src/tablet-webpage/main.js
@@ -3,87 +3,158 @@ var app = new Vue({
   // storing the state of the page
   data: {
     connected: false,
+    loading: true,
     ros: null,
-    ws_address: "",
+    ws_address: "ws://0.0.0.0:9000",
     logs: [],
     topic: null,
-    message: null
+    message: null,
+    dragging: false,
+    x: "no",
+    y: "no",
+    dragCircleStyle: {
+      margin: "0px",
+      top: "0px",
+      left: "0px",
+      display: "none",
+      width: "75px",
+      height: "75px",
+    },
+    // joystick valules
+    joystick: {
+      vertical: 0,
+      horizontal: 0,
+    },
+    //publisher
+    pubInterval: null,
   },
   // helper methods to connect to ROS
   methods: {
     connect: function () {
       console.log("connecting to rosbridge server");
-      this.logs.unshift('Connecting to rosbridge server');
+      this.logs.unshift("Connecting to rosbridge server");
       this.ros = new ROSLIB.Ros({
         url: this.ws_address,
       });
       this.ros.on("connection", () => {
         this.connected = true;
-        this.logs.unshift('Connected to rosbridge server');
-        // console.log("Connected!");
+        this.loading = false;
+        this.logs.unshift("Connected to rosbridge server");
+        this.pubInterval = setInterval(this.joyPublish, 100)
       });
       this.ros.on("error", (error) => {
-          this.logs.unshift("Error connecting to rosbridge server");
-        // console.log("Error connecting to websocket server: ", error);
+        this.logs.unshift("Error connecting to rosbridge server");
       });
       this.ros.on("close", () => {
         this.connected = false;
         this.logs.unshift("Disconnected from rosbridge server");
-        // console.log("Connection to websocket server closed.");
-      })
+        clearInterval(this.pubInterval)
+      });
     },
     disconnect: function () {
-        this.ros.close();
+      this.ros.close();
     },
-    setTopic: function(){
-        this.topic = new ROSLIB.Topic({
-          ros: this.ros,
-          name: '/cmd_vel',
-          messageType: 'geometry_msgs/Twist'
-        })
+    setTopic: function () {
+      this.topic = new ROSLIB.Topic({
+        ros: this.ros,
+        name: "/cmd_vel",
+        messageType: "geometry_msgs/Twist",
+      });
     },
-    forward: function(){
-        this.message = new ROSLIB.Message({
-          linear: { x: 1, y: 0, z: 0 },
-          angular: { x: 0, y: 0, z: 0},
-        })
-        this.setTopic()
-        this.topic.publish(this.message)
+    forward: function () {
+      this.message = new ROSLIB.Message({
+        linear: { x: 1, y: 0, z: 0 },
+        angular: { x: 0, y: 0, z: 0 },
+      });
+      this.setTopic();
+      this.topic.publish(this.message);
     },
-    stop: function(){
+    stop: function () {
       this.message = new ROSLIB.Message({
         linear: { x: 0, y: 0, z: 0 },
-        angular: { x: 0, y: 0, z: 0},
-      })
-      this.setTopic()
-      this.topic.publish(this.message)
+        angular: { x: 0, y: 0, z: 0 },
+      });
+      this.setTopic();
+      this.topic.publish(this.message);
     },
-    backward: function(){
+    backward: function () {
       this.message = new ROSLIB.Message({
         linear: { x: -1, y: 0, z: 0 },
-        angular: { x: 0, y: 0, z: 0},
-      })
-      this.setTopic()
-      this.topic.publish(this.message)
+        angular: { x: 0, y: 0, z: 0 },
+      });
+      this.setTopic();
+      this.topic.publish(this.message);
     },
-    turnLeft: function(){
+    turnLeft: function () {
       this.message = new ROSLIB.Message({
         linear: { x: 0.5, y: 0, z: 0 },
-        angular: { x: 0, y: 0, z: 0.5},
-      })
-      this.setTopic()
-      this.topic.publish(this.message)
+        angular: { x: 0, y: 0, z: 0.5 },
+      });
+      this.setTopic();
+      this.topic.publish(this.message);
     },
-    turnRight: function(){
+    turnRight: function () {
       this.message = new ROSLIB.Message({
         linear: { x: 0.5, y: 0, z: 0 },
-        angular: { x: 0, y: 0, z: -0.5},
-      })
-      this.setTopic()
-      this.topic.publish(this.message)
+        angular: { x: 0, y: 0, z: -0.5 },
+      });
+      this.setTopic();
+      this.topic.publish(this.message);
     },
     clearLogs: function () {
-        this.logs = [];
-        }
+      this.logs = [];
+    },
+    joyPublish: function () {
+      let topic = new ROSLIB.Topic({
+        ros: this.ros,
+        name: "/cmd_vel",
+        messageType: "geometry_msgs/Twist",
+      });
+      let message = new ROSLIB.Message({
+        linear: { x: this.joystick.vertical, y: 0, z: 0 },
+        angular: { x: 0, y: 0, z: this.joystick.horizontal },
+      });
+      console.log('publish joy')
+      topic.publish(message);
+    },
+    startDrag() {
+      this.dragging = true;
+      this.x = this.y = 0;
+    },
+    stopDrag() {
+      this.dragging = false;
+      this.x = this.y = "no";
+      this.dragCircleStyle.display = "none";
+      this.resetJoystickVals();
+    },
+    doDrag(event) {
+      if (this.dragging) {
+        this.x = event.offsetX;
+        this.y = event.offsetY;
+        let ref = document.getElementById("dragstartzone");
+        this.dragCircleStyle.display = "inline-block";
+
+        let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2;
+        let maxTop = minTop + 200;
+        let top = this.y + minTop;
+        this.dragCircleStyle.top = `${top}px`;
+
+        let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2;
+        let maxLeft = minLeft + 200;
+        let left = this.x + minLeft;
+        this.dragCircleStyle.left = `${left}px`;
+
+        this.setJoystickVals();
+      }
+    },
+    setJoystickVals() {
+      this.joystick.vertical = -1 * (this.y / 200 - 0.5);
+      this.joystick.horizontal = +1 * (this.x / 200 - 0.5);
+      this.joyPublish()
+    },
+    resetJoystickVals() {
+      this.joystick.vertical = 0;
+      this.joystick.horizontal = 0;
+    },
   },
 });
