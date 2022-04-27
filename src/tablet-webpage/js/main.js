9a817b71c207773a362e470e8c20372ca8ae51b9
dylanhoover <dylangranthoover@gmail.com>
2022-04-27 13:47:26 -0700

    local js and css files, tabs for modes

--- src/tablet-webpage/js/main.js
+++ src/tablet-webpage/js/main.js
@@ -156,5 +156,18 @@ var app = new Vue({
       this.joystick.vertical = 0;
       this.joystick.horizontal = 0;
     },
+    openTab: function(evt, mode) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(mode).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
   },
 });
