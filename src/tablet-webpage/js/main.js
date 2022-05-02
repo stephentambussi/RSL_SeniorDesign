da92f69c3254d7c88a886d2712fa4c8411699e3b
central-station <stevetambussi@gmail.com>
2022-05-01 19:03:08 -0700

    pushing dylans changes

--- src/tablet-webpage/js/main.js
+++ src/tablet-webpage/js/main.js
@@ -217,18 +217,18 @@ var app = new Vue({
       evt.currentTarget.className += " active";
     },
     setCamera: function () {
-      let without_wss = this.rosbridge_address.split("wss://")[1];
-      console.log(without_wss);
-      let domain = without_wss.split("/")[0] + "/" + without_wss.split("/")[1];
-      console.log(domain);
-      let host = domain + "/cameras";
+      // let without_wss = this.rosbridge_address.split("wss://")[1];
+      // console.log(without_wss);
+      // let domain = without_wss.split("/")[0] + "/" + without_wss.split("/")[1];
+      // console.log(domain);
+      // let host = domain + "/cameras";
       let viewer = new MJPEGCANVAS.Viewer({
         divID: "divCamera",
-        host: host,
+        host: 'localhost:8080',
         width: 320,
         height: 240,
-        topic: "/camera/rgb/image_raw",
-        ssl: true,
+        topic: "/zed2i/zed_node/rgb/image_rect_color",
+        ssl: false,
       });
     },
   },
