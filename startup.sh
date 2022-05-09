b5f27516b37763bb22d57bc5368a720ce36b11f6
central-station <stevetambussi@gmail.com>
2022-05-08 21:33:36 -0700

    Created startup script for interface

--- startup.sh
+++ startup.sh
@@ -8,4 +8,6 @@ gnome-terminal --working-directory=/home/central-station/senior_design_ws -- ros
 sleep 5
 gnome-terminal --working-directory=/home/central-station/senior_design_ws -- rosrun navigator mode_switcher.py
 gnome-terminal --working-directory=/home/central-station/senior_design_ws -- rosrun web_video_server web_video_server
-gnome-terminal --working-directory=/home/central-station/senior_design_ws/src/tablet-webpage -- live-server --port=5500
+#gnome-terminal --working-directory=/home/central-station/senior_design_ws/src/tablet-webpage -- live-server --port=5500
+cd /home/central-station/senior_design_ws/src/tablet-webpage
+live-server --port=5500
