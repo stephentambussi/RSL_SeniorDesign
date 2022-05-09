1c81fcae7765a793490169bb863f6ee20e8a8ca7
central-station <stevetambussi@gmail.com>
2022-05-08 20:52:59 -0700

    Created startup script for interface

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#This will start up the robot for running everything
+source /opt/ros/melodic/setup.bash
+#source catkin_ws/devel/setup.bash
+source /home/central-station/senior_design_ws/devel/setup.bash
+
+gnome-terminal --working-directory=/home/central-station/senior_design_ws -- roslaunch rosbridge_server rosbridge_websocket.launch
+
+sleep 5
+gnome-terminal --working-directory=/home/central-station/senior_design_ws -- rosrun navigator mode_switcher.py
+gnome-terminal --working-directory=/home/central-station/senior_design_ws -- rosrun web_video_server web_video_server
+gnome-terminal --working-directory=/home/central-station/senior_design_ws/src/tablet-webpage -- live-server --port=5500
