e74f460623a3c3c3f53b014f63950495296c378e
central-station <stevetambussi@gmail.com>
2022-05-10 13:39:36 -0700

    Made the interface boot on launch

--- live-server_startup.sh
+++ live-server_startup.sh
@@ -4,4 +4,5 @@ source /opt/ros/melodic/setup.bash
 source /home/central-station/senior_design_ws/devel/setup.bash
 #cd /home/central-station/senior_design_ws/src/tablet-webpage
 
-gnome-terminal --working-directory=/home/central-station/senior_design_ws/src/tablet-webpage -- live-server --port=5500
+#gnome-terminal --working-directory=/home/central-station/senior_design_ws/src/tablet-webpage -- live-server --port=5500
+gnome-terminal -- /bin/bash -c "cd /home/central-station/senior_design_ws/src/tablet-webpage;/home/central-station/.nvm/versions/node/v14.4.0/bin/live-server --port=5500"
